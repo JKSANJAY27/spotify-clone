89e9f0bb5e032a37ac735735bcfc18b19921b5e5
JKSANJAY27 <j.k.sanjay2006@gmail.com>
2024-10-25 22:32:37 +0530

    modified:   spotify-admin/src/App.jsx 	new file:   spotify-admin/src/assets/add_album.png 	new file:   spotify-admin/src/assets/add_song.png 	new file:   spotify-admin/src/assets/album_icon.png 	new file:   spotify-admin/src/assets/assets.js 	new file:   spotify-admin/src/assets/logo.png 	new file:   spotify-admin/src/assets/logo_small.png 	new file:   spotify-admin/src/assets/song_icon.png 	new file:   spotify-admin/src/components/Navbar.jsx 	new file:   spotify-admin/src/components/Sidebar.jsx 	modified:   spotify-admin/src/main.jsx

---
+++ spotify-admin/src/assets/assets.js
@@ -0,0 +1,15 @@
+import logo from "./logo.png";
+import logosmall from "./logo_small.png";
+import addsong from "./add_song.png";
+import song from "./song_icon.png";
+import addalbum from "./add_album.png";
+import album from "./album_icon.png";
+
+export const assets = {
+    logo,
+    logosmall,
+    addsong,
+    song,
+    addalbum,
+    album
+}
