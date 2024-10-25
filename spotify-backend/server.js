4a79e5755798e39e0bfb51504616a7d1ed00051f
JKSANJAY27 <j.k.sanjay2006@gmail.com>
2024-10-25 22:00:01 +0530

    modified:   spotify-backend/server.js 	new file:   spotify-backend/src/controllers/albumController.js 	modified:   spotify-backend/src/controllers/songController.js 	new file:   spotify-backend/src/routes/albumRoute.js 	modified:   spotify-backend/src/routes/songRoute.js

--- spotify-backend/server.js
+++ spotify-backend/server.js
@@ -4,6 +4,7 @@ import 'dotenv/config'
 import songRouter from "./src/routes/songRoute.js";
 import connectDB from "./src/config/mongodb.js";
 import connectCloudinary from "./src/config/cloudinary.js";
+import albumRouter from "./src/routes/albumRoute.js";
 
 const app = express();
 const port = process.env.PORT || 4000;
@@ -14,6 +15,7 @@ app.use(express.json());
 app.use(cors());
 
 app.use("/api/song", songRouter)
+app.use("/api/album", albumRouter)
 
 app.get('/',(req,res)=> res.send("API Sucessful"))
 
